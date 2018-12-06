57493af443aa9ded7abbb0d34c4c0ea29ab2eb43
MrGuacamole <37054689+MrGuacamole@users.noreply.github.com>
2018-12-07 11:12:43 +1300

    Add Solidity Test

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,15 +5,40 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
+ 
+
+    function testOwner() public {
+        SupplyChain supplyChain = new SupplyChain();
+        Assert.equal(supplyChain.owner(), this, "Deployer is different from owner");
+    }
 
     // Test for failing conditions in this contracts
     // test that every modifier is working
+     // function testForSale() public{
+        // SupplyChain supplyChain = new SupplyChain();
+        // supplyChain.addItem("Book", 200);
+        // Assert.equal(supplyChain.items(1).State, supplyChain.State.ForSale , "Not for sale");
+
+
+        // _item = new supplyChain.Item();
+        // supplyChain.Item _item = supplyChain.items(0);
+        // Assert.equal(supplyChain.items[0].state.ForSale(), supplyChain.items(0).state.ForSale(), "boop bop");
+     // }
 
     // buyItem
 
     // test for failure if user does not send enough funds
+    // function enoughFunds(){
+    //     Assert.equal(supplyChainTest.buyItem(0))
+    // }
     // test for purchasing an item that is not for Sale
+    // function notForSale(){
+    //    var _item = supplyChainTest.getMapping(0);
 
+    //     _item.state = supplyChainTest.State.Sold;
+    //     Assert.equal(_item.state, supplyChainTest.State.Sold, "Purchase should fail");
+
+    // }
 
     // shipItem
 
@@ -24,7 +49,13 @@ contract TestSupplyChain {
 
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
-
+    
+        // function testNotMarkedShipped(){
+        //     SupplyChain supplyChain = new SupplyChain();
+        //     supplyChain.addItem("Book", 200);
+            
+        //     Assert.fail(supplyChain.receiveItem(1));
+        // }
      
 
 
