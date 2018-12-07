189f3e43f80661eb5ceb3a13aa922c13acc07fec
MrGuacamole <37054689+MrGuacamole@users.noreply.github.com>
2018-12-07 13:04:41 +1300

    Add more tests

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -5,58 +5,47 @@ import "truffle/DeployedAddresses.sol";
 import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
- 
+    SupplyChain supplyChain = new SupplyChain();
 
     function testOwner() public {
-        SupplyChain supplyChain = new SupplyChain();
         Assert.equal(supplyChain.owner(), this, "Deployer is different from owner");
     }
 
     // Test for failing conditions in this contracts
     // test that every modifier is working
-     // function testForSale() public{
-        // SupplyChain supplyChain = new SupplyChain();
-        // supplyChain.addItem("Book", 200);
-        // Assert.equal(supplyChain.items(1).State, supplyChain.State.ForSale , "Not for sale");
-
-
-        // _item = new supplyChain.Item();
-        // supplyChain.Item _item = supplyChain.items(0);
-        // Assert.equal(supplyChain.items[0].state.ForSale(), supplyChain.items(0).state.ForSale(), "boop bop");
-     // }
 
     // buyItem
 
     // test for failure if user does not send enough funds
-    // function enoughFunds(){
-    //     Assert.equal(supplyChainTest.buyItem(0))
-    // }
+   
     // test for purchasing an item that is not for Sale
-    // function notForSale(){
-    //    var _item = supplyChainTest.getMapping(0);
-
-    //     _item.state = supplyChainTest.State.Sold;
-    //     Assert.equal(_item.state, supplyChainTest.State.Sold, "Purchase should fail");
-
-    // }
+    function testNotForSale(){
+        address(supplyChain).call(abi.encodeWithSignature("addItem(uint256)"), "book", 0);
+        address(supplyChain).call(abi.encodeWithSignature("buyItem(uint256)"), 0);
+        bool result = address(supplyChain).call(abi.encodeWithSignature("buyItem(uint256)"), 0);
+        Assert.isFalse(result, "failed to check if for sale");
+    }
 
     // shipItem
 
     // test for calls that are made by not the seller
+    function testNonSeller(){
+            address(supplyChain).call(abi.encodeWithSignature("addItem(uint256)"), "Car", 1);
+            bool result = address(supplyChain).call(abi.encodeWithSignature("shipItem(uint256)"), 1);
+            Assert.isFalse(result, "failed to verify caller");
+        }
+        
     // test for trying to ship an item that is not marked Sold
+    function testIfShipped(){
+    
+            bool result = address(supplyChain).call(abi.encodeWithSignature("shipItem(uint256)"), 1);
+            Assert.isFalse(result, "failed to check if sold");
+        }
 
     // receiveItem
-
+        
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
-    
-        // function testNotMarkedShipped(){
-        //     SupplyChain supplyChain = new SupplyChain();
-        //     supplyChain.addItem("Book", 200);
-            
-        //     Assert.fail(supplyChain.receiveItem(1));
-        // }
-     
 
 
 }
